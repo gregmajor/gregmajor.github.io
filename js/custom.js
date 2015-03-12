115330193ba5361d0ee63d7ba8c83c96616b2928
Greg Major <greg.b.major@gmail.com>
2015-03-12 15:13:29 -0500

    Style updates

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,16 @@
-
 /*Begin custom.js file
 azee - Minimal Blog theme for Jekyll  main JS file
  *  owwwlab.com @2014
  *  owwwlab@gmail.com
 ----------------------------------------------*/
+
 (function($) {
     "use strict";
 
     var $emptyHeaders=$('.blog-no-header').parents('.post-header');
+    
     $emptyHeaders.parents('.post-item').addClass('no-header');
+    
     $emptyHeaders.remove();
 
     var $contactwrapper=$('#contact-wrapper');
@@ -17,6 +19,7 @@ azee - Minimal Blog theme for Jekyll  main JS file
     	e.preventDefault();
     	$contactwrapper.animate({top:0},300);
     })
+    
     $('#contact-close').on('click',function(e){
     	e.preventDefault();
     	$contactwrapper.animate({top:'100%'},300);
